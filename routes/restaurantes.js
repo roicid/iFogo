74ee51b5527cb5e964d7ad59696800a1a1eb5dc0
roicid <roi_cid89@hotmail.com>
2020-10-31 18:47:55 +0100

    boton search

--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -4,15 +4,16 @@ const restaurantes =require('../models/restaurantes')
 const withAuth = require('../helpers/middleware');
 
 
-router.get('/buscadorPrincipal' ,(req , res , next ) => {
-
-    
-        restaurantes.find()
+router.get('/buscadorPrincipal:/id' ,(req , res , next ) => {
+        restaurantes.find(req.query.search)
         .then((result) => {
-            console.log(result)
+            console.log(result.body.nombre[0])
         }).catch((err) => {
             console.log(err)
         });
 
 
 }) 
+
+
+module.exports = router
