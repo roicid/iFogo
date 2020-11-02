0927c51abdeea0cc8f0ac87466808b29555d8431
Alex <alexgarcia1771@outlook.com>
2020-11-02 11:24:45 +0100

    alex

--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -6,8 +6,6 @@ const withAuth = require('../helpers/middleware');
 // consultar a dani para mostrar varios !! 
 //problemas con el json 
 
-<<<<<<< HEAD
-=======
 
 router.get('/buscadorPrincipal' ,(req , res , next ) => {
   //  const {Nombre , Direccion} = req.body
@@ -21,25 +19,8 @@ router.get('/buscadorPrincipal' ,(req , res , next ) => {
         }).catch((err) => {
             console.log(err)
         });
->>>>>>> d0a24d45f18a28cfc0fc6da57ff23bce7763ac32
 
 
-<<<<<<< HEAD
-=======
-})    
-
-router.get('/restaurantes' ,(req , res , next) => {
-    try {
-        res.render('restaurantes'); 
-        
-    } catch (error) {
-        console.log(error)
-        
-    }
-})
-
-
->>>>>>> ac47d63da614a24f112ff2f4dbcbc4b5ecca85a9
 
 
 module.exports = router
