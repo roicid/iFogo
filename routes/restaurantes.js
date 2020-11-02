1a9ce53e300d868f9c6e75c5a81e2c264fd87f79
Alex <alexgarcia1771@outlook.com>
2020-11-02 11:08:02 +0100

    Alex

--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -1,29 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const restaurantes =require('../models/restaurantes')
+const Restaurante =require('../models/restaurantes');
 const withAuth = require('../helpers/middleware');
 
-// consultar a dani para mostrar varios !! 
-//problemas con el json 
-
-<<<<<<< HEAD
-=======
 
 router.get('/buscadorPrincipal' ,(req , res , next ) => {
-  //  const {Nombre , Direccion} = req.body
-        restaurantes.find().limit(3)
-        
+ 
+        Restaurante.find().limit(3)
         .then((result) => {
-          
-    
-            res.render('index' , {data : result} )
-           console.log(result);
+            res.render('index' , {data : result})
+           console.log(result)
         }).catch((err) => {
             console.log(err)
         });
->>>>>>> d0a24d45f18a28cfc0fc6da57ff23bce7763ac32
-
-
-
+    });
 
 module.exports = router
