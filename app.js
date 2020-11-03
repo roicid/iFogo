186a2b4ebfde6a302ea2c8b67f6d39a8f247438a
Alex <alexgarcia1771@outlook.com>
2020-11-03 17:13:38 +0100

    rama-alex

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 const mongoose = require("mongoose");
 
 mongoose
