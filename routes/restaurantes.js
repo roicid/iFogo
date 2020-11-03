aa9cb64aad50fd0abbfa3da976d3fef690a91824
Alex <alexgarcia1771@outlook.com>
2020-11-03 11:40:06 +0100

    master

--- routes/restaurantes.js
+++ routes/restaurantes.js
@@ -1,45 +1,61 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Restaurante =require('../models/restaurantes')
-const withAuth = require('../helpers/middleware');
-const uploadCloud = require('../config/cloudinary')
+const Restaurante = require("../models/restaurantes");
+const withAuth = require("../helpers/middleware");
+const uploadCloud = require("../config/cloudinary");
 
-// consultar a dani para mostrar varios !! 
-//problemas con el json 
+// consultar a dani para mostrar varios !!
+//problemas con el json
 
-
-router.get('/buscadorPrincipal' , async (req , res , next ) => {
+router.get("/buscadorPrincipal", async (req, res, next) => {
   //  const {Nombre , Direccion} = req.body
-      
-        
 
-        try {
-            const consulta = await Restaurante.find({Nombre : {$regex: req.query.buscador, $options:"i"}});
-            //console.log(req.query.buscador)
-           // console.log(consulta);
-            res.render('index' , {data : consulta} )
-           
-        
-            
-        } catch (error) {
-            console.log(error)
-            
-        }
-          
-    
-            
+  try {
+    const consulta = await Restaurante.find({
+      Nombre: { $regex: req.query.buscador, $options: "i" },
+    }).limit(5);
+    //console.log(req.query.buscador)
+    // console.log(consulta);
+    res.render("index", { data: consulta });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get("/restaurantes", (req, res, next) => {
+  try {
+    res.render("restaurantes");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-})    
+router.post(
+  "/restaurantesform",
+  uploadCloud.single("Foto1"),
+  async (req, res, next) => {
+    const { Nombre, Direccion, URLReal, Email } = req.body;
+    const Foto1 = req.file.url;
+    const imgName = req.file.originalname;
 
-router.get('/restaurantes' ,(req , res , next) => {
     try {
-        res.render('restaurantes'); 
-        
+      const nuevoRestaurante = await Restaurante.create({
+        Nombre,
+        Direccion,
+        URLReal,
+        Foto1,
+        Email,
+        imgName,
+      });
+
+      res.render("bienvenidoRestaurante", { nuevoRestaurante });
     } catch (error) {
-        console.log(error)
-        
+      console.log(error);
     }
+
+  }
+);
+
 })
 
 router.post('/restaurantesform', uploadCloud.single('Foto1'), async(req,res,next) =>  {
@@ -58,6 +74,4 @@ try {
 }
 })
 
-
-
-module.exports = router
+module.exports = router;
