99689b02fdafc34a0bac1bd1c38113329a1f971c
roicid <roi_cid89@hotmail.com>
2020-11-03 12:02:23 +0100

    ok

--- models/restaurantes.js
+++ models/restaurantes.js
@@ -1,33 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const restauranteSchema = new Schema(
-  {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    Email: String,
-    password: String
-=======
-=======
-    
->>>>>>> master
-=======
-    
->>>>>>> rama-alex
-    Nombre: String,
-    Direccion: String,
-    Email: String,
-    Foto1: String,
-    URLReal:String
-    
+const restauranteSchema = new Schema({
 
-  },
-  {
-    timestamps: true
-  }
-);
+
+  Password: String,
+  Nombre: String,
+  Direccion: String,
+  Email: String,
+  Foto1: String,
+  URLReal: String
+
+
+}, {
+  timestamps: true
+});
 
 const Restaurante = mongoose.model('Restaurante', restauranteSchema);
 
